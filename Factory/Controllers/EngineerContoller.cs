d9eeea5ce0d94f6facb284ae9d6c220a752991df
Sean Keane <seankeane93@gmail.com>
2022-08-12 12:08:02 -0700

    Continue to add dependencies in .csproj, Program, and Startup files.  The application successfully builds.

---
+++ Factory/Controllers/EngineerContoller.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+  }
+}
